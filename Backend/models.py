de659b86f860bc05c35983ea767cd16806cbdf25
Ajit Chauhan <52689606+AjitChauhan081@users.noreply.github.com>
2025-12-27 16:34:50 +0530

    Refactor models and add User and EquipmentCategory classes

--- Backend/models.py
+++ Backend/models.py
@@ -1,10 +1,41 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
-from datetime import date,datetime
-from sqlalchemy import Date,DateTime
+from sqlalchemy import String, ForeignKey, Boolean, Date, Float,DateTime
 from database import Base
-from datetime import date
+from datetime import date, datetime
 
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column(String(255),unique=True, index=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    
+
+    
+class EquipmentCategory(Base):
+    __tablename__ = "equipment_category"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(unique=True) # e.g., Computers, Monitors [cite: 9]
+
+
+class Equipment(Base):
+    __tablename__ = "equipment"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    Serial_number: Mapped[str] = mapped_column(unique=True) 
+    category_id: Mapped[int] = mapped_column(ForeignKey("equipment_category.id"))
+    Company : Mapped[str] = mapped_column(index=True)
+    Used_by:Mapped[str] = mapped_column(index=True)
+    department: Mapped[str] = mapped_column() 
+    Location: Mapped[str] = mapped_column() 
+    technician_id: Mapped[int] = mapped_column(ForeignKey("user.id")) # Default technician
+    Equipment_name : Mapped[str] = mapped_column()
+    Purchase_Date : Mapped[date] = mapped_column(Date)
+    Assigned_Date : Mapped[date] = mapped_column(Date)
+    Warranty_Info : Mapped[str] = mapped_column()
+    is_usable: Mapped[bool] = mapped_column(default=True) # For Scrap logic [cite: 72]
+    team_id: Mapped[int] = mapped_column(ForeignKey("maintenance_team.id"))
+    Description: Mapped[str] = mapped_column()
 
 class MaintenanceTeam(Base):
     __tablename__ = "maintenance_team" # Fixed typo [cite: 20]
@@ -38,16 +69,3 @@ class RequestForm(Base):
     Notes : Mapped[str] = mapped_column()
     Instruction : Mapped[str] = mapped_column()
     
-
-class Equipment(Base):
-    __tablename__ = "equipment"
-    id : Mapped[int] = mapped_column(primary_key=True, index=True)
-    Equipment_name : Mapped[str] = mapped_column()
-    Serial_Number : Mapped[str] = mapped_column(unique=True,index=True)
-    Company : Mapped[str] = mapped_column(index=True)
-    Purchase_Date : Mapped[date] = mapped_column(Date)
-    Warranty_Info : Mapped[str] = mapped_column()
-    Location : Mapped[str] = mapped_column()
-    
-
-
